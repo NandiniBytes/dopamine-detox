ef54d1c20c81401e9457248e0c32f8221d8be30c
Nandini <22bcm042@nirmauni.ac.in>
2025-07-20 12:37:59 +0530

    Delete app directory

--- app/schemas/recommendation.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-class Recommendation(BaseModel):
-    activity_name: str
-    description: str
-    category: str # e.g., "Creative", "Outdoor", "Mindfulness"
